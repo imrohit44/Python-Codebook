222a0008b7e47db77a24e4f6b8c7f1af92362a66
Rohit Mohan <rohitmohan7804@gmail.com>
2025-08-24 23:00:27 +0530

    Add files via upload

---
+++ program_110.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key in self.cache:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+            return value
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) >= self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+
+if __name__ == '__main__':
+    lru_cache = LRUCache(2)
+    lru_cache.put(1, 1)
+    lru_cache.put(2, 2)
+    print(lru_cache.get(1))
+    lru_cache.put(3, 3)
+    print(lru_cache.get(2))
